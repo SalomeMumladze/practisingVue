433d19309ae43e3ab342c36173febd513cac9eb0
salome <salomemumladze775@gmail.com>
2021-09-18 13:45:13 +0400

    add component, slot

---
+++ src/componentsSlots/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <button @click="selectComponent='appQuote'">Quote</button>
+        <button @click="selectComponent='appAutor'">Author</button>
+        <button @click="selectComponent='appNew'">New</button>
+        <p>{{selectComponent}}</p>
+        <keep-alive>
+          <!-- this keep-alive tag kept alive this components not destroied-->
+          <component :is="selectComponent">
+            <p>default component</p>
+          </component>
+        </keep-alive>
+        <!-- <app-quote>
+          <p slot="title">hello</p>
+          <h2 slot="subTitle">{{quoteTitle}}</h2>
+        </app-quote> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue'
+import Author from './components/Author.vue'
+import New from './components/New.vue'
+  export default {
+    data: function(){
+      return {
+        quoteTitle: "wonderfull title",
+        selectComponent: 'appQuote'
+      }
+    },
+    components: {
+      appQuote: Quote,
+      appNew: New,
+      appAutor: Author
+    }
+  }
+</script>
---
+++ src/componentsSlots/src/components/New.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    and New
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+
+</style>
